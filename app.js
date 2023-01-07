f220a56afbf69fe85a352b77c81e4011fb124a34
amulya-parab <amulyaparab101@gmail.com>
2023-01-07 16:24:43 +0530

    edited port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server is up and running");
 });
 
